6fea2a022b6152e74c891349770edd9dad34dd7d
zdlomo023 <zdlomo023@student.wethinkcode.co.za>
2024-06-25 14:21:05 +0200

    Displayed information extracted

--- nedbank/student_accounts.py
+++ nedbank/student_accounts.py
@@ -11,21 +11,18 @@ with open("nedbankMigoals.html", "w", encoding='utf-8') as file:
     file.write(soup.prettify())
 
 
-#     youth_accounts = youth_accounts_section.find_all('div', class_='product-card-wrapper')  # Adjust class name if necessary
-def get_basics(soup):
-    title = soup.find('h1').text.strip()
-    description = soup.find('p').text.strip()
-    monthly_fee = soup.find('h3').text.strip()
+def get_basics():
+    migoal = soup.find('div', class_='nbd-credit-card-banner-content')
+    title = migoal.find('h1').text.strip()
+    description = migoal.find('p').text.strip()
+    monthly_fee = migoal.find('h3').text.strip()
+    print_basics(title, description, monthly_fee)
+    return title, description, monthly_fee
+
+def print_basics(title, description, monthly_fee):
     print(f"Title: {title}")
     print(f"Description: {description}")
     print(f"Maintenance Fee: {monthly_fee}")
-    return title, description, monthly_fee
 
-def get_details(soup):
-    details = soup.find('div', class_='nbd-credit-card-banner-content')
-    if details:
-        title, description, monthly_fee = get_basics(details)
-        return title, description, monthly_fee
-    else:
-        print("MyMo Bank Account not found on the page.")
-        return None, None, None
+if __name__ == '__main__':
+    get_basics()
