2ba652d9b89f51932df7fb195131347f0ecff31d
zdlomo023 <zdlomo023@student.wethinkcode.co.za>
2024-06-26 10:42:33 +0200

    Scraped the benefits of migoals account

--- nedbank/student_accounts.py
+++ nedbank/student_accounts.py
@@ -6,11 +6,6 @@ html = requests.get(url)
 
 soup = BeautifulSoup(html.content, 'html.parser')
 
-
-with open("nedbankMigoals.html", "w", encoding='utf-8') as file:
-    file.write(soup.prettify())
-
-
 def get_basics():
     migoal = soup.find('div', class_='nbd-credit-card-banner-content')
     title = migoal.find('h1').text.strip()
@@ -19,10 +14,42 @@ def get_basics():
     print_basics(title, description, monthly_fee)
     return title, description, monthly_fee
 
+
+def get_account_benefits():
+    sections = soup.find_all('div', class_='card-body m-0 p-0')
+    categories = {
+        'Rates and fees': [],
+        'Rewards and discounts': [],
+        'Convenience and control': [],
+        'Security': []
+    }
+
+    for section in sections:
+        title = section.find('h5', class_='card-title nb-card-heading').text.strip()
+        items = section.find_all('p')
+        category_info = [item.text.strip() for item in items if item.text.strip()]
+        
+        if title in categories:
+            categories[title] = category_info
+
+    print_rewards(categories)
+
+
 def print_basics(title, description, monthly_fee):
     print(f"Title: {title}")
     print(f"Description: {description}")
     print(f"Maintenance Fee: {monthly_fee}")
 
+def print_rewards(categories):
+    for category, info in categories.items():
+        print(f"\n{category}:")
+        for item in info:
+            print(f"- {item}")
+
+def print_perks(perks):
+    print(f"\nPerks: {perks}")
+
 if __name__ == '__main__':
     get_basics()
+    get_account_benefits()
+   
