0b6311bdb49f9aed07e4bdbe3214de0fe00a64bd
zdlomo023 <zdlomo023@student.wethinkcode.co.za>
2024-07-03 16:14:54 +0200

    Included test for account benefits

--- nedbank/test_student_accounts.py
+++ nedbank/test_student_accounts.py
@@ -19,7 +19,41 @@ class TestMigoalsAccountFunctions(unittest.TestCase):
         self.assertEqual(title, "MiGoals")
         self.assertEqual(description, "For anytime, anywhere banking on your terms, get MiGoals.")
         self.assertEqual(monthly_fee, "R5")
-   
+
+
+
+    def test_get_account_benefits(self):
+        mock_html = """
+            <div class="card-body m-0 p-0">
+                <h5 class="card-title nb-card-heading">Rates and fees</h5>
+                <p>Pay R5 monthly maintenance fees.</p>
+                <p>Get free in-app transaction notifications.</p>
+                <p>Add Greenbacks membership for only R30 a month.</p>
+            </div>
+            <div class="card-body m-0 p-0">
+                <h5 class="card-title nb-card-heading">Rewards and discounts</h5>
+                <p>Cashback on purchases</p>
+                <p>Discounts on movie tickets</p>
+            </div>
+        """
+
+        # self.assertEqual(len(benefit_categories['Rates and fees']), 2)
+        # self.assertEqual(len(benefit_categories['Rewards and discounts']), 2)
+
+        soup = BeautifulSoup(mock_html, 'html.parser')
+        benefit_categories = get_account_benefits(soup)
+
+        self.assertEqual(benefit_categories["Rates and fees"], [
+            "Pay R5 monthly maintenance fees.",
+            "Get free in-app transaction notifications.",
+            "Add Greenbacks membership for only R30 a month."
+        ])
+        self.assertEqual(len(benefit_categories['Rates and fees']), 3)
+
+
+
+    def test_get_account_perks(self):
+        pass
 
 
 
